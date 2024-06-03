ca8e8e37c0c7bba9980539a965d34f51e23cae17
ange.saavedra17@gmail.com <ange.saavedra17@gmail.com>
2024-06-03 07:08:10 +0200

    Rails simple airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,20 @@
+require 'faker'
+
+Flat.destroy_all
+puts 'Creating 5 flats...'
+
+5.times do
+  Flat.create!(
+    name: Faker::Address.unique.community,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph,
+    price_per_night: rand(50..200),
+    number_of_guests: rand(1..6),
+    image_url: 'https://source.unsplash.com/random/800x600?house'
+  )
+end
+puts 'Finished !'
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
